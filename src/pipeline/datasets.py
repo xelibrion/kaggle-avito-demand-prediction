c1ebd1f3c64e0e958eecbdadba1e0c1e3eacd8ef
Dmitry Kryuchkov <dkryuchkov@seek.com.au>
2018-05-15 22:34:28 +0100

    package target along with features

--- src/pipeline/datasets.py
+++ src/pipeline/datasets.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import luigi
 from sklearn.externals import joblib
 import h5py
@@ -20,13 +21,14 @@ class FeatureHandling(luigi.Task):
 class ComposeDataset(luigi.Task):
     fold_idx = luigi.IntParameter()
     features = luigi.Parameter()
+    target_column = luigi.Parameter()
 
     def requires(self):
         feature_steps = [
             FeatureHandling(feature_name=x) for x in self.features.split(',')
         ]
         return {
-            'train': TrainSet(),
+            'raw_data': TrainSet(),
             'folds': CreateTrainFolds(),
             'features': feature_steps,
         }
@@ -34,35 +36,51 @@ class ComposeDataset(luigi.Task):
     def output(self):
         return luigi.LocalTarget(f'data_{self.fold_idx}.h5')
 
-    def run(self):
+    def _load_features(self):
         feature_sets = []
         for f_task in self.input()['features']:
             feature_sets.append(joblib.load(f_task.path))
 
+        return np.hstack(feature_sets)
+
+    def _get_train_test(self):
         with h5py.File(self.input()['folds'].path, 'r') as in_file:
             train_idx = in_file['train'][f'fold_{self.fold_idx}'].value
             test_idx = in_file['test'][f'fold_{self.fold_idx}'].value
+            return train_idx, test_idx
 
-        full_set = np.hstack(feature_sets)
-        train_set = full_set[train_idx]
-        test_set = full_set[test_idx]
+    def _write_dataset(self, label, features, target, out_file):
+        grp = out_file.create_group(label)
+        grp.create_dataset(
+            'features',
+            features.shape,
+            data=features,
+            compression='gzip',
+            compression_opts=1,
+        )
+        grp.create_dataset(
+            'target',
+            target.shape,
+            data=target,
+            compression='gzip',
+            compression_opts=1,
+        )
+
+    def run(self):
+        features = self._load_features()
+        train_idx, test_idx = self._get_train_test()
+
+        df = pd.read_csv(self.input()['raw_data'].path)
+        target_values = df[self.target_column].values
+
+        train_set = features[train_idx]
+        train_set_target = target_values[train_idx]
+        test_set = features[test_idx]
+        test_set_target = target_values[test_idx]
 
         out_file = h5py.File(self.output().path, 'w')
-        train_grp = out_file.create_group('train')
-        train_grp.create_dataset(
-            'features',
-            train_set.shape,
-            data=train_set,
-            compression='gzip',
-        )
-
-        test_grp = out_file.create_group('test')
-        test_grp.create_dataset(
-            'features',
-            test_set.shape,
-            data=test_set,
-            compression='gzip',
-        )
+        self._write_dataset('train', train_set, train_set_target, out_file)
+        self._write_dataset('test', test_set, test_set_target, out_file)
 
         out_file.close()
 
@@ -73,15 +91,14 @@ class TrainOnFold(luigi.Task):
     target = luigi.Parameter()
 
     def requires(self):
-        return {
-            'features': ComposeDataset(
-                fold_idx=self.fold_idx,
-                features=self.features,
-            )
-        }
+        return ComposeDataset(
+            fold_idx=self.fold_idx,
+            features=self.features,
+            target_column=self.target,
+        )
 
     def output(self):
-        pass
+        return luigi.LocalTarget('model.xgb')
 
     def run(self):
         print('xgb.train')
--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -12,31 +12,16 @@ logging.basicConfig(
 
 logging.getLogger("luigi.scheduler").setLevel(logging.WARNING)
 
-
-class AllInOne(luigi.Task):
-    def requires(self):
-        yield TrainOnFold()
-
-    def complete(self):
-        return all([x.complete() for x in self.requires()])
-
-
 if __name__ == '__main__':
     setup_interface_logging.has_run = True
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--task', default='AllInOne')
     parser.add_argument('--fold', required=True, type=int)
     parser.add_argument('--features', default='region,city')
     parser.add_argument('--target', default='deal_probability')
 
-    args, unknown = parser.parse_known_args()
+    args = parser.parse_args()
 
-    luigi.build(
-        [TrainOnFold(
-            fold_idx=args.fold,
-            features=args.features,
-            target=args.target,
-        )],
-        workers=12,
-        local_scheduler=True)
+    tasks = [TrainOnFold(fold_idx=args.fold, features=args.features, target=args.target)]
+
+    luigi.build(tasks, workers=12, local_scheduler=True)
